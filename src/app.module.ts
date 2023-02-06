b851b08117c823adba68e32b5feb5f1fd38acc68
WebToLaure <lmarmus@hotmail.fr>
2023-02-06 22:00:56 +0100

    creation passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [],
+@Module({//imports servent à communiquer avec la database
+  imports: [
+    ConfigModule.forRoot(),// c'est quoi?
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      logging: false
+    }),
+
+
+
+
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { constructor(private datasource: DataSource) { } }
+//app.module: module racine du projet
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap() {//fonction qui démarre notre appli
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+ app.useGlobalPipes(new ValidationPipe({
+
+    whitelist: true,
+    forbidNonWhitelisted: true,
+
+  }));
+
+
+
+
+  await app.listen(3000);//démarre l'écoute http et attend les requêtes
 }
 bootstrap();
+//Le fichier d'entrée de l'application qui utilise la fonction principale NestFactory pour créer une instance d'application Nest.npm 
