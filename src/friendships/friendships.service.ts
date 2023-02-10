8f545fdc5e53d229e53e592ba7150a7551da6ebd
Captainbigleu <jessie.favre@live.fr>
2023-02-10 09:45:15 +0100

    feat swagger

--- src/friendships/friendships.service.ts
+++ src/friendships/friendships.service.ts
@@ -8,7 +8,6 @@ export class FriendshipsService {
     return await Friendship.create({ user, friend }).save();
   }
 
-<<<<<<< HEAD
   
   async findOne(id: number) {
     return await Friendship.findOneBy({id});
@@ -18,21 +17,6 @@ export class FriendshipsService {
     const friendship = await Friendship.findOneBy({id});
     friendship.accepted = true;
     return friendship.save()
-=======
-
-  async findOne(id: number) {
-    return await Friendship.findOne({ relations: { friend: true, user: true }, where: { id } });
-  }
-
-  async update(id: number) {
-    const friendship = await Friendship.findOneBy({ id });
-    friendship.accepted = true;
-    return friendship.save();
-  }
-
-  async findByUserAndFriend(user:User, friend:User){
-    return await Friendship.findOne({ relations: { friend: true, user: true }, where: { user: {id: user.id}, friend: {id: friend.id}} });
->>>>>>> 56fcb653cb688678d01ac3311f52a3f343d8cdff
   }
 
   async remove(id: number) {
